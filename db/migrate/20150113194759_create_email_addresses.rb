eb9df30cf8b5051bbd406795c78a997b3164f5bf
chenwei <chenwei@china-ihop.com>
2015-10-08 12:28:46 +0800

    add

---
+++ db/migrate/20150113194759_create_email_addresses.rb
@@ -0,0 +1,13 @@
+class CreateEmailAddresses < ActiveRecord::Migration
+  def change
+    drop_table :email_addresses if table_exists? :email_addresses
+    create_table :email_addresses do |t|
+      t.column :user_id, :integer, :null => false
+      t.column :address, :string, :null => false
+      t.column :is_default, :boolean, :null => false, :default => false
+      t.column :notify, :boolean, :null => false, :default => true
+      t.column :created_on, :timestamp, :null => false
+      t.column :updated_on, :timestamp, :null => false
+    end
+  end
+end
